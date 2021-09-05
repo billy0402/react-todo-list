2612aef3e9b91c9887d0e99d734ddf080319e9be
Billy Huang <10646003@ntub.edu.tw>
2021-09-05 22:19:49 +0800

    feat(TodoList): title

--- src/views/TodoList/TodoList.tsx
+++ src/views/TodoList/TodoList.tsx
@@ -5,7 +5,14 @@ import styles from './index.scss';
 const TodoList = () => {
   return (
     <div className={styles.layout}>
-      <div className={styles.todoListWrapper}>TodoList</div>
+      <div className={styles.todoListWrapper}>
+        <div className={styles.header}>
+          <span className={styles.title}>Todo List</span>
+          <span className={styles.subTitle}>
+            Hi here is a sample todo list.
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/views/TodoList/index.scss.d.ts
+++ src/views/TodoList/index.scss.d.ts
@@ -1,2 +1,5 @@
+export const header: string;
 export const layout: string;
+export const subTitle: string;
+export const title: string;
 export const todoListWrapper: string;
