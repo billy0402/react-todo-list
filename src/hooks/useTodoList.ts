b5ce6914c75797b4bdcfe298dc0ee59bf5fd72c0
Billy Huang <10646003@ntub.edu.tw>
2021-09-05 21:53:24 +0800

    feat(useTodoList): according page setup event logic

--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -5,7 +5,48 @@ import { Todo } from '../types/todoList';
 const useTodoList = () => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
 
-  return { todoList } as const;
+  const addTodo = (todo: Todo) => {
+    setTodoList([...todoList, todo]);
+  };
+
+  const switchTodoIsDoneStatus = (id: number) => {
+    const targetTodoIndex = todoList.findIndex((todo: Todo) => todo.id === id);
+
+    const newTodoList = [...todoList];
+
+    newTodoList[targetTodoIndex] = {
+      ...newTodoList[targetTodoIndex],
+      isDone: !newTodoList[targetTodoIndex].isDone,
+    };
+
+    setTodoList(newTodoList);
+  };
+
+  const deleteTodo = (id: number) => {
+    const newTodoList = todoList.filter((todo: Todo) => todo.id !== id);
+
+    setTodoList(newTodoList);
+  };
+
+  const [filterIsDoneTodo, setFilterIsDoneTodo] = useState<boolean>(false);
+
+  const switchFilterIsDoneTodo = () => {
+    setFilterIsDoneTodo(!filterIsDoneTodo);
+  };
+
+  let workTodoList = todoList;
+  if (filterIsDoneTodo) {
+    workTodoList = todoList.filter((todo) => !todo.isDone);
+  }
+
+  return {
+    todoList: workTodoList,
+    addTodo,
+    switchTodoIsDoneStatus,
+    deleteTodo,
+    filterIsDoneTodo,
+    switchFilterIsDoneTodo,
+  } as const;
 };
 
 export default useTodoList;
